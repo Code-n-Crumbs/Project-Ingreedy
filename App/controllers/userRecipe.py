a5ea7e906c64580b0c1da034cc691a3fd550a7f1
AnuRam03 <anuradha.ramlakhan03@gmail.com>
2025-05-04 03:44:08 -0400

    Update userRecipe.py

--- App/controllers/userRecipe.py
+++ App/controllers/userRecipe.py
@@ -1,3 +1,12 @@
 from App.models import *
 from App.database import db
 
+def get_user_recipe(user_id, recipe_id):
+    user_recipe = UserRecipe.query.filter_by(user_id=user_id,recipe_id=recipe_id).first()
+    if user_recipe:
+        return user_recipe
+    else:
+        return None
+    
+def get_all_user_recipes(user_id):
+    return UserRecipe.query.filter_by(user_id=user_id).all()
