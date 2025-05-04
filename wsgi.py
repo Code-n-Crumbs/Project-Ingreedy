c23ce97d413033f948aa40002bae3715ff16c532
AnuRam03 <anuradha.ramlakhan03@gmail.com>
2025-05-04 04:22:51 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ from flask.cli import with_appcontext, AppGroup
 from App.database import db, get_migrate
 from App.models import User
 from App.main import create_app
-from App.controllers import ( create_user, get_all_users_json, get_all_users, initialize )
+from App.controllers import *
 
 
 # This commands file allow you to create convenient CLI commands for testing controllers
