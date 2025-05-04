68c43097fcac99fbf5e94b8c27146467c7d74aa8
AnuRam03 <anuradha.ramlakhan03@gmail.com>
2025-05-04 03:47:22 -0400

    Update ingredient.py

--- App/models/ingredient.py
+++ App/models/ingredient.py
@@ -4,15 +4,14 @@ class Ingredient(db.Model):
     ingredient_id = db.Column(db.Integer, primary_key=True)
     ingredient_name = db.Column(db.String(225), nullable=False)
     ingredient_recipe = db.relationship('RecipeIngredient', backref='ingredient', lazy=True) #RI has an ingredient pseudo-column
-    ingredient_unit = db.Column(db.String(64), nullable=False)
+    #ingredient_unit = db.Column(db.String(64), nullable=False) #unit is removed for simplification
 
-    def __init__(self, name, unit,):
+    def __init__(self, name):
         self.ingredient_name = name
-        self.ingredient_unit = unit
+        #self.ingredient_unit = unit
 
     def get_json(self):
         return{
             "ingredient_id": self.ingredient_id,
-            "ingredient_name": self.ingredient_name,
-            "ingredient_unit": self.ingredient_unit
+            "ingredient_name": self.ingredient_name
         }
